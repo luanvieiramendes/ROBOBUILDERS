5910d792b6d74cb1218b243a9fe40d7a8b5e9a65
ANTONY GMAIL <ANTONY@LOCAL>
2026-08-21 14:31:09 -0300

    tag

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #pragma once
 // Versão do firmware - incremente a cada release para OTA detectar
-#define FIRMWARE_VERSION "1.2.0"
+#define FIRMWARE_VERSION "1.3.0"
 #define FIRMWARE_VERSION_CODE 120 // 1.2.0 -> 120
 // URL base do repo para OTA (publico, sem token)
 #define GITHUB_REPO "antony1727/ESTAGIO-"
