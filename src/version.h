77451163c50ef44662073fc8121404484de1a99b
ANTONY GMAIL <ANTONY@LOCAL>
2026-08-21 14:48:54 -0300

    chore: bump 2.0.0

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 #pragma once
 // Versão do firmware - incremente a cada release para OTA detectar
-#define FIRMWARE_VERSION "1.5.0"
-#define FIRMWARE_VERSION_CODE 120 // 1.2.0 -> 120
+#define FIRMWARE_VERSION "2.0.0"
+#define FIRMWARE_VERSION_CODE 200 // 2.0.0 -> 200
 // URL base do repo para OTA (publico, sem token)
 #define GITHUB_REPO "antony1727/ESTAGIO-"
 #define GITHUB_API_LATEST "https://api.github.com/repos/antony1727/ESTAGIO-/releases/latest"
